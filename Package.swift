68fbf51a6f2a022d67b93965086e37157fa59c2f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:25:49 +0100

    Update dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .library(name: "HTML Standard Elements", targets: ["HTML Standard Elements"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-whatwg-html", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-whatwg-html", from: "0.1.1"),
     ],
     targets: [
         .target(
